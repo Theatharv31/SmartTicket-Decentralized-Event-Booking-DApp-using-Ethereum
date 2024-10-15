b722f9fd707a80db702bbeea0e7951a9c5ba4a37
Theatharv31 <isapureatharv2003@gmail.com>
2024-10-15 09:21:57 +0530

    Project Completed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,62 +7,62 @@ const tokens = (n) => {
 async function main() {
   // Setup accounts & variables
   const [deployer] = await ethers.getSigners()
-  const NAME = "TokenMaster"
-  const SYMBOL = "TM"
+  const NAME = "SmartTicket"
+  const SYMBOL = "ST"
 
   // Deploy contract
-  const TokenMaster = await ethers.getContractFactory("TokenMaster")
-  const tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
-  await tokenMaster.deployed()
+  const SmartTicket = await ethers.getContractFactory("SmartTicket")
+  const smartTicket = await SmartTicket.deploy(NAME, SYMBOL)
+  await smartTicket.deployed()
 
-  console.log(`Deployed TokenMaster Contract at: ${tokenMaster.address}\n`)
+  console.log(`Deployed SmartTicket Contract at: ${smartTicket.address}\n`)
 
   // List 6 events
   const occasions = [
     {
-      name: "UFC Miami",
+      name: "Venture Capital World Summit",
       cost: tokens(3),
       tickets: 0,
-      date: "May 31",
+      date: "Oct 23",
       time: "6:00PM EST",
-      location: "Miami-Dade Arena - Miami, FL"
+      location: "Bengaluru, India"
     },
     {
-      name: "ETH Tokyo",
+      name: "Bussiness EXPO & Summit",
       cost: tokens(1),
       tickets: 125,
-      date: "Jun 2",
+      date: "Oct 26-27",
       time: "1:00PM JST",
-      location: "Tokyo, Japan"
+      location: "Mumbai, India"
     },
     {
-      name: "ETH Privacy Hackathon",
+      name: "TOKEN2049",
       cost: tokens(0.25),
       tickets: 200,
       date: "Jun 9",
       time: "10:00AM TRT",
-      location: "Turkey, Istanbul"
+      location: "Marina Bay Sands,Singpore"
     },
     {
-      name: "Dallas Mavericks vs. San Antonio Spurs",
+      name: "Machine Learning summit",
       cost: tokens(5),
       tickets: 0,
-      date: "Jun 11",
+      date: "Oct 17",
       time: "2:30PM CST",
-      location: "American Airlines Center - Dallas, TX"
+      location: "Bengaluru, India"
     },
     {
-      name: "ETH Global Toronto",
+      name: "ETH Global",
       cost: tokens(1.5),
       tickets: 125,
       date: "Jun 23",
-      time: "11:00AM EST",
-      location: "Toronto, Canada"
+      time: "9:00AM EST",
+      location: "Dubai"
     }
   ]
 
   for (var i = 0; i < 5; i++) {
-    const transaction = await tokenMaster.connect(deployer).list(
+    const transaction = await smartTicket.connect(deployer).list(
       occasions[i].name,
       occasions[i].cost,
       occasions[i].tickets,
